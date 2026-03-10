47bfdeee98a9869c95b0b20a04e978c43ecb555f
Nipun Nirman <164348907+nipunnirman@users.noreply.github.com>
2026-03-10 09:24:48 +0530

    add jwt

--- src/app/api.py
+++ src/app/api.py
@@ -27,6 +27,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+from .routers.auth import router as auth_router
+app.include_router(auth_router)
+
 @app.get("/")
 async def root():
     return {
